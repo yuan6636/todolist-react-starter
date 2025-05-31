2f76d239407f027a7c9099f636102bc8247383d8
yuan6636 <aleniverson777@gmail.com>
2025-05-31 21:28:17 +0800

    feat: add todo input props

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -67,7 +67,7 @@ const StyledAddTodoActionContainer = styled.div`
     }
   }
 `;
-const TodoInput = () => {
+const TodoInput = ({ inputValue, onChange, onKeyDown, onAddTodo }) => {
   return (
     <StyledAddTodoContainer>
       <StyledLabelIcon className="icon" htmlFor="add-todo-input" />
