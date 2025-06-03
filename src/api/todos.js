b2d527d0645a80b84d1b46f9f1834cac7cf627b9
yuan6636 <aleniverson777@gmail.com>
2025-06-03 12:02:15 +0800

    feat: add todo crud function

---
+++ src/api/todos.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3004'
+
+export const getTodos = () => {}
+
+export const createTodo = () => {}
+
+export const patchTodo = () => {}
+
+export const deleteTodo = () => {}
