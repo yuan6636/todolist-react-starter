7731becd6dfef81c72c558374a0bfb175152f1f5
yuan6636 <aleniverson777@gmail.com>
2025-06-03 12:05:36 +0800

    feat: implement get todos

--- src/api/todos.js
+++ src/api/todos.js
@@ -2,7 +2,14 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3004'
 
-export const getTodos = () => {}
+export const getTodos = async() => {
+  try {
+    const res = await axios.get(`${baseUrl}/todos`)
+    return res.data
+  } catch (error) {
+    console.error('[Get Todos failed]: ', error)
+  }
+}
 
 export const createTodo = () => {}
 
