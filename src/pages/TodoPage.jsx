fd8fc5e3a4e3033e0cdde8a25d8c1f4e4595e1d5
yuan6636 <aleniverson777@gmail.com>
2025-06-03 15:44:22 +0800

    feat: add useEffect to get todos from api

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -1,32 +1,10 @@
 import { Footer, Header, TodoCollection, TodoInput } from 'components';
-import { useState } from 'react'
-
-const dummyTodos = [
-  {
-    title: 'Learn react-router',
-    isDone: true,
-    id: 1,
-  },
-  {
-    title: 'Learn to create custom hooks',
-    isDone: false,
-    id: 2,
-  },
-  {
-    title: 'Learn to use context',
-    isDone: true,
-    id: 3,
-  },
-  {
-    title: 'Learn to implement auth',
-    isDone: false,
-    id: 4,
-  },
-];
+import { useState, useEffect } from 'react'
+import { getTodos } from '../api/todos'
 
 const TodoPage = () => {
   const [inputValue, setInputValue] = useState('')
-  const [todos, setTodos] = useState(dummyTodos)
+  const [todos, setTodos] = useState([])
 
   const todoNums = todos.length
 
@@ -118,6 +96,19 @@ const TodoPage = () => {
   const handleDelete = (id) => {
     setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id))
   }
+
+  useEffect(() => {
+    const getTodosAsync = async () => {
+      try {
+        const todos = await getTodos()
+        
+        setTodos(todos.map((todo) => ({ ...todo, isEdit: false })))
+      } catch (error) {
+        console.error(error)
+      }
+    }
+    getTodosAsync()
+  }, [])
 
   return (
     <div>
