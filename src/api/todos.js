a11ba1c06881a679801f7aec91a34642d4892927
yuan6636 <aleniverson777@gmail.com>
2025-06-03 16:16:53 +0800

    feat: add update todo api

--- src/api/todos.js
+++ src/api/todos.js
@@ -25,6 +25,18 @@ export const createTodo = async (payload) => {
   }
 }
 
-export const patchTodo = () => {}
+export const patchTodo = async (payload) => {
+  const { id, title, isDone } = payload
+  try {
+    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+      title,
+      isDone
+    })
+    
+    return res.data
+  } catch (error) {
+    console.error('[Patch Todo failed]: ', error)
+  }
+}
 
 export const deleteTodo = () => {}
