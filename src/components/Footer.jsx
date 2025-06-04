d8cd5923a4158be323551951c3db244f395aa727
yuan6636 <aleniverson777@gmail.com>
2025-06-04 16:29:15 +0800

    feat: add logout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 
 const StyledFooter = styled.footer`
   display: flex;
@@ -32,10 +33,17 @@ const StyledButton = styled.button`
 `;
 
 const Footer = ({ numOfTodos }) => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    localStorage.removeItem('authToken')
+    navigate('/login')
+  }
+
   return (
     <StyledFooter>
       <p>剩餘項目數： {numOfTodos}</p>
-      <StyledButton>登出</StyledButton>
+      <StyledButton onClick={handleClick}>登出</StyledButton>
     </StyledFooter>
   );
 };
