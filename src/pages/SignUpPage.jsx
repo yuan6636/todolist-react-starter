3139a85879d9771a2351e392b0d05a990acf1185
yuan6636 <aleniverson777@gmail.com>
2025-06-04 15:20:05 +0800

    feat: add register function to signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -8,12 +8,51 @@ import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
+import { register } from '../api/auth';
 
 const SignUpPage = () => {
   const [username, setUsername] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
- 
+  
+  const handleClick = async () => {
+    if (username.length === 0) {
+      return
+    }
+    if (email.length === 0) {
+      return 
+    }
+    if (password.length === 0) {
+      return
+    }
+
+    const { success, authToken } = await register({ 
+      username, 
+      email, 
+      password 
+    })
+
+    if (success) {
+      localStorage.setItem('authToken', authToken)
+      Swal.fire({
+        position: 'top',
+        title: '註冊成功!',
+        timer: 1000,
+        icon: 'success',
+        showConfirmButton: false
+      })
+      return
+    }
+    Swal.fire({
+      position: 'top',
+      title: '註冊失敗!',
+      timer: 1000,
+      icon: 'error',
+      showConfirmButton: false
+    })
+  }
+
   return (
     <AuthContainer>
       <div>
@@ -48,7 +87,7 @@ const SignUpPage = () => {
           onChange={(passwordInputValue) => setPassword(passwordInputValue)}
         />
       </AuthInputContainer>
-      <AuthButton>註冊</AuthButton>
+      <AuthButton onClick={handleClick}>註冊</AuthButton>
       <Link to="/login">
         <AuthLinkText>取消</AuthLinkText>
       </Link>
