b2738d6e5dfc2518103d0f53e2978f3362c4a7a0
yuan6636 <aleniverson777@gmail.com>
2025-06-04 11:00:44 +0800

    feat: add authinput props at signup page

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -6,8 +6,13 @@ import {
 } from 'components/common/auth.styled';
 import { ACLogoIcon } from 'assets/images';
 import { AuthInput } from 'components';
+import { useState } from 'react';
 
 const SignUpPage = () => {
+  const [username, setUsername] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+ 
   return (
     <AuthContainer>
       <div>
@@ -16,15 +21,31 @@ const SignUpPage = () => {
       <h1>建立您的帳號</h1>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="帳號"
+          placeholder="請輸入帳號"
+          value={username}
+          onChange={(nameInputValue) => setUsername(nameInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          label="Email"
+          placeholder="請輸入 email"
+          value={email}
+          onChange={(emailInputValue) => setEmail(emailInputValue)}
+        />
       </AuthInputContainer>
 
       <AuthInputContainer>
-        <AuthInput />
+        <AuthInput
+          type="password"
+          label="密碼"
+          placeholder="請輸入密碼"
+          value={password}
+          onChange={(passwordInputValue) => setPassword(passwordInputValue)}
+        />
       </AuthInputContainer>
       <AuthButton>註冊</AuthButton>
       <AuthLinkText>取消</AuthLinkText>
