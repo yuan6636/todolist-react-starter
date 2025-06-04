df8a6cb65d945fc331f9a69f638c8b5faec5aa70
yuan6636 <aleniverson777@gmail.com>
2025-06-04 12:41:44 +0800

    feat: show info alerts

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,6 +9,7 @@ import { AuthInput } from 'components';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { login } from '../api/auth'; 
+import Swal from 'sweetalert2';
 
 const LoginPage = () => {
   const [username, setUsername] = useState('')
@@ -28,9 +29,24 @@ const LoginPage = () => {
     })
     if (success) {
       localStorage.setItem('authToken', authToken)
+      Swal.fire({
+        position: 'top',
+        title: '登入成功!',
+        timer: 1000,
+        icon: 'success',
+        showConfirmButton: false
+      })
+      return
     }
+    Swal.fire({
+      position: 'top',
+      title: '登入失敗!',
+      timer: 1000,
+      icon: 'error',
+      showConfirmButton: false
+    })
   }
-  
+
   return (
     <AuthContainer>
       <div>
