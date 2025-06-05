11f9c6789a140e4a8aa643b7c0b7b569c90fad68
yuan6636 <aleniverson777@gmail.com>
2025-06-05 12:14:41 +0800

    feat: add auth function to context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,4 +1,6 @@
-import { createContext } from 'react'
+import { login, register } from '../api/auth';
+import { createContext, useState } from 'react'
+import * as jwt from 'jsonwebtoken'
 
 const defaultAuthContext = {
   isAuthenticated: false,
@@ -9,3 +11,59 @@ const defaultAuthContext = {
 }
 
 const AuthContext = createContext(defaultAuthContext)
+
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+  const [payload, setPayload] = useState(null)
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        currentMember: payload && {
+          id: payload.sub,
+          name: payload.name
+        },
+        register: async (data) => {
+          const { success, authToken } = await register({ 
+            username: data.username,
+            email: data.email,
+            password: data.password
+          })
+          const tempPayload = jwt.decode(authToken)
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success
+        },
+        login: async (data) => {
+          const { success, authToken } = await login({ 
+            username: data.username,
+            password: data.password
+          })
+          const tempPayload = jwt.decode(authToken)
+          if (tempPayload) {
+            setPayload(tempPayload)
+            setIsAuthenticated(true)
+            localStorage.setItem('authToken', authToken)
+          } else {
+            setPayload(null)
+            setIsAuthenticated(false)
+          }
+          return success
+        },
+        logout: () => {
+          localStorage.removeItem('authToken')
+          setPayload(null)
+          setIsAuthenticated(false)
+        }
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
